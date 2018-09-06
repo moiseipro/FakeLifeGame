c943566dee12cf10bbb7960692cde9d32b4757fc
Sadlize <sadlize@mail.ru>
2018-09-06 13:50:20 +0300

    DoorOpen Syste,

---
+++ FakeLifeGame/Assets/DoorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    private Animator _animator;
+
+    void Start () {
+        _animator = GetComponent<Animator>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player") _animator.SetBool("IsDoorOpen", true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
